ff6d599efafed3b29b9e1312916c9f3d44027dbc
gopi krishna <ygkyak@gmail.com>
2024-04-08 19:05:45 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { CONTACT_DATA } from "./constants";
-import ContactCard from "./components/ContactCard";
-import Hedder from "./components/Hedder";
+import './App.css';
+import { CONTACT_DATA } from './constants';
+import ContactCard from './components/ContactCard';
+import Hedder from './components/Hedder';
 
 function App() {
   const innerWidth = window.innerWidth;
 
   return (
     <>
-      <Hedder />
-      <div className="pageWrapper">
-        <div className={`${innerWidth > 600 ? "gridBox" : "listBox"}`}>
-          {CONTACT_DATA?.length > 0 &&
-            CONTACT_DATA?.map((item) => {
-              return <ContactCard item={item} />;
-            })}
-        </div>
+    
+    <Hedder/>
+    <div className='pageWrapper'>
+      <div className={`${innerWidth > 600 ? "gridBox" : "listBox"}`}>
+      {
+        CONTACT_DATA?.length > 0 && 
+        CONTACT_DATA?.map((item) => {
+          return (
+            <ContactCard item={item} />
+          )
+        })
+      }
       </div>
+    </div>
     </>
   );
 }
